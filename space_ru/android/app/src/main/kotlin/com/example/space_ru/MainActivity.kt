31baf80db7988f7a8067b478cfd81ab511836a4b
valeriy.chernousov <valeriy.chernousov@ebs.ru>
2024-12-18 18:13:06 +0300

    init bloc tamplate

---
+++ space_ru/android/app/src/main/kotlin/com/example/space_ru/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.space_ru
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ space_ru/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/widgets.dart';
+import 'package:space_ru/app.dart';
+
+void main() => runApp(const App());
